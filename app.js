f889b2b185c7e40d2f3526617da05afd977f4480
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-27 18:53:09 -0600

    this one has proper syntax and should work

--- app.js
+++ app.js
@@ -60,32 +60,27 @@ $('document').ready(function() {
     //         $bit.append(exchange + " Current Bid: $" + bid);
     //
     //     });
-}
-// console.log(currentPrice);
-// //use the setInterval method to the api every minute
-// setInterval(currentPrice, interval);
+    // }
+    // console.log(currentPrice);
+    // //use the setInterval method to the api every minute
+    // setInterval(currentPrice, interval);
+    var webAuth = new auth0.WebAuth({
+        domain: "guitarlanguage.auth0.com",
+        clientID: "y8AA4z9Gre45oDzkbPIjJTM4RnE36HJG",
+        redirectUri: "https://cocky-sinoussi-e4da0e.netlify.com/",
+        audience: 'https://' + "guitarlanguage.auth0.com" + '/userinfo',
+        responseType: 'token id_token',
+        scope: 'openid'
+    });
 
+    var loginBtn = $('#authLogin');
 
-});
-
-// http://localhost:3000,
-
-var webAuth = new auth0.WebAuth({
-    domain: "guitarlanguage.auth0.com",
-    clientID: "y8AA4z9Gre45oDzkbPIjJTM4RnE36HJG",
-    redirectUri: "https://cocky-sinoussi-e4da0e.netlify.com/",
-    audience: 'https://' + "guitarlanguage.auth0.com" + '/userinfo',
-    responseType: 'token id_token',
-    scope: 'openid'
-});
-
-var loginBtn = $('#authLogin');
-
-loginBtn.click(function(e) {
-e.preventDefault();
-webAuth.authorize();
-});
+    loginBtn.click(function(e) {
+        e.preventDefault();
+        webAuth.authorize();
+    });
 
 });
+
 
 // http://localhost:3000,
