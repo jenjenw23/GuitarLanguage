3de837df36a87f6ac30d13dd7050583f10e482b4
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-27 23:19:52 -0600

    examining color schemes

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ $('document').ready(function() {
             options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
         }
     });
-    // caching jquery object
+    // caching jquery objects
     var $bit = $("#bit-div");
+    var $bitAsk = $("#bit-ask");
+    var $bitBid = $("#bit-bid");
 
     //this function connects to the bitcoin weighted average api and
     //prints it to the $bit div
@@ -46,11 +48,37 @@ $('document').ready(function() {
 
         });
     }
-    sevenDayWeightedAvg();
+    console.log(sevenDayWeightedAvg);
 
-    //set interval for ajax repeated call
-    var interval = 60000;
-        // use the setInterval method to call sevenDayWeightedAvg every 1 min
-    setInterval(sevenDayWeightedAvg, interval);
+    //calls bitcoincharts api for current ask and bid price from bitstamp exchange
+    function currentPrice() {
+        var queryURL = "http://api.bitcoincharts.com/v1/markets.json";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            var results = response;
+
+            var obj = JSON.parse(results);
+            console.log(obj);
+
+            // Printing the entire object to console
+            var ask = obj["57"]["ask"];
+            var bid = obj["57"]["bid"];
+            var exchange = obj["57"]["symbol"];
+
+            // Constructing HTML containing the artist information
+            $bitAsk.html(exchange + " Asking Price: $" + ask + "<br>");
+            $bitBid.html(exchange + " Current Bid: $" + bid);
+
+        });
+    }
+
+    sevenDayWeightedAvg();
+    currentPrice();
+    setInterval(sevenDayWeightedAvg, 60000);
+    // var interval = 60000;
+    // //use the setInterval method to the api every minute
+    setInterval(currentPrice, 60000);
 
 });
